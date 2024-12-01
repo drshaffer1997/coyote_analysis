cafeac0b8f3dced0d14758b8005e28f06c63ce40
Dakotah Shaffer <dakotahrshaffer@gmail.com>
2024-12-01 15:51:35 -0500

    Data wrangling to get data frames in the right format. There is more copying and pasting than is advisable so far... I am going to try to use loops and functions for accuracy and reproducibility.

--- 03_Scripts/import_and_clean_data.R
+++ 03_Scripts/import_and_clean_data.R
@@ -6,7 +6,12 @@ library(tidyr)
 library(ggplot2)
 library(lubridate)
 
+# Import data from retrieved gps collars
 
 collar_id_154955 <- read_csv("01_Raw_data/PinPoint 154955 2024-10-17 10-11-29.csv", skip = 4)
 collar_id_154963 <- read_csv("01_Raw_data/PinPoint 154963 2024-08-02 11-02-41.csv", skip = 4)
 collar_id_154964 <- read_csv("01_Raw_data/PinPoint 154964 2024-06-11 14-11-47.csv", skip = 4)
+
+
+# Checking structure and format of data frames
+str(collar_id_154955)
