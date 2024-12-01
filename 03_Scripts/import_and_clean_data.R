5fee4e72c863021e30a1d628f4e28b698abc974d
Dakotah Shaffer <dakotahrshaffer@gmail.com>
2024-12-01 15:17:57 -0500

    Imported data from 3 recovered gps collars.

---
+++ 03_Scripts/import_and_clean_data.R
@@ -0,0 +1,12 @@
+##### Importing and Cleaning Coyote Movement Data #####
+
+library(dplyr)
+library(readr)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+
+
+collar_id_154955 <- read_csv("01_Raw_data/PinPoint 154955 2024-10-17 10-11-29.csv", skip = 4)
+collar_id_154963 <- read_csv("01_Raw_data/PinPoint 154963 2024-08-02 11-02-41.csv", skip = 4)
+collar_id_154964 <- read_csv("01_Raw_data/PinPoint 154964 2024-06-11 14-11-47.csv", skip = 4)
