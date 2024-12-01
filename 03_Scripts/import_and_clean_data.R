bfbb6b246d0e88be158f7d0072855c576c4e8cc5
Dakotah Shaffer <dakotahrshaffer@gmail.com>
2024-12-01 16:13:22 -0500

    My loop to pull all data files that start with "PinPoint" is not working. I adapted it from Data Carpentry. I believe the problem is setting the file path. I will set up a meeting with Geraldine to troubleshoot.

--- 03_Scripts/import_and_clean_data.R
+++ 03_Scripts/import_and_clean_data.R
@@ -12,6 +12,39 @@ collar_id_154955 <- read_csv("01_Raw_data/PinPoint 154955 2024-10-17 10-11-29.cs
 collar_id_154963 <- read_csv("01_Raw_data/PinPoint 154963 2024-08-02 11-02-41.csv", skip = 4)
 collar_id_154964 <- read_csv("01_Raw_data/PinPoint 154964 2024-06-11 14-11-47.csv", skip = 4)
 
+# Create loop to import and merge gps collar data
+
+deluca_files <- list.files(path = "01_Raw_data/deluca", pattern = "PinPoint")
+
+num_files <- length(deluca_files)
+
+for (i in 1:num_files){
+  filename <- deluca_files[i]
+  data <- read_csv("01_Raw_data/deluca")   ######## code currently is not working... meet with Geraldine###
+}
+
+data
+# Add column with gps id
+
+collar_id_154955 <- collar_id_154955 |> 
+  mutate(collar_id = "154955")
+
+collar_id_154963 <- collar_id_154955 |> 
+  mutate(collar_id = "154963")
+
+collar_id_154964 <- collar_id_154955 |> 
+  mutate(collar_id = "154964")
+
+# Merge data frames for each collar
+
+all_collar_data <- rbind(collar_id_154955, collar_id_154963, collar_id_154964)
 
 # Checking structure and format of data frames
-str(collar_id_154955)
+str(all_collar_data)
+
+
+# Convert timestamp column into correct format
+
+all_collar_data <- all_collar_data |> 
+  rename(gmt_date_time = `GMT Time`) |> 
+  mutate(gmt_date_time = as.POSIXct(gmt_date_time, "%m/%d/%Y %H:%M:%S", tz="GMT"))
